f7049a45f8c9dfdb432898983edc2aaaebc2d11f
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-02-23 23:59:15 +0000

    refactor: separate fetch recipe function

---
+++ src/fetchRecipe.js
@@ -0,0 +1,9 @@
+const fetchRecipe = async () => {
+  const APP_ID = "a3ad555a"
+  const API_KEY = "b9c79644c2f78df9d76f77fc33c0fa24"
+  const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${API_KEY}`).then(response => response.json())
+  console.log(response.hits)
+
+}
+
+export default fetchRecipe;
