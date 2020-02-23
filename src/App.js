91799bd95054d45607dab38bbf596b1b108041ea
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-02-23 23:33:48 +0000

    feat: add search box into css

--- src/App.js
+++ src/App.js
@@ -3,11 +3,21 @@ import './App.css';
 
 const App = () => {
 
+  const website = `https://api.edamam.com/search?q=chicken&app_id=${process.env.APP_ID}&app_key=${process.env.API_KEY}`
 
   return (
-
+      < div className="MainApp" >
     <h1> Hello world </h1>
-
+    <form className="search-form">  
+      <input className="search-input" type ="text" />
+      <button
+      className="search-button" 
+      type="submit"
+      >
+      Search
+      </button>
+    </form>
+    </div>
   );
 
 
