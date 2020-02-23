170f67a4125f40dd6848319615e135fed9f8c27b
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-02-23 23:34:30 +0000

    Chore: add useEffect on load page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 const App = () => {
 
   const website = `https://api.edamam.com/search?q=chicken&app_id=${process.env.APP_ID}&app_key=${process.env.API_KEY}`
 
+  const [counter, setCounter] = useState(0)
+  
+  useEffect(() => {
+  console.log('effect has been run')
+  })
+  
   return (
       < div className="MainApp" >
     <h1> Hello world </h1>
