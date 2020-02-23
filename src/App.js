8df3334f9ecdb9655c103f166c2068e3195bc303
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-02-23 23:51:08 +0000

    Feat: fetch data from recipe API

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import './App.css';
 
 const App = () => {
 
+  const APP_ID = "a3ad555a"
+  const API_KEY = "b9c79644c2f78df9d76f77fc33c0fa24"
   const website = `https://api.edamam.com/search?q=chicken&app_id=${process.env.APP_ID}&app_key=${process.env.API_KEY}`
-
-  const [counter, setCounter] = useState(0)
   
   useEffect(() => {
-  console.log('effect has been run')
+ fetchRecipe()
   })
   
+  const fetchRecipe = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${API_KEY}`)
+    const data = response.json()
+    console.log(data)
+
+  }
+
+
   return (
       < div className="MainApp" >
     <h1> Hello world </h1>
