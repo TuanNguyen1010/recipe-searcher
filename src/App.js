0e205c7aff18c6ba81c2c46b3fe68ac344233109
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-02-24 23:09:00 +0000

    Feat: search recipe function added

--- src/App.js
+++ src/App.js
@@ -8,27 +8,37 @@ const App = () => {
 
   const [recipes, setRecipe] = useState([]);
   const [search, setSearch] = useState("")
+  const [query, setQuery] = useState('chicken')
 
   useEffect(() => {
   fetchRecipe()
-  })
+  }, [query])
 
   const fetchRecipe = async () => {
     const APP_ID = "a3ad555a"
     const API_KEY = "b9c79644c2f78df9d76f77fc33c0fa24"
-    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${API_KEY}`).then(response => response.json())
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${API_KEY}`).then(response => response.json())
     setRecipe(response.hits)
     console.log(response.hits)
   }
   
   const newSearch = e => {
     setSearch(e.target.value)
+    console.log(search)
+  }
+
+  const getSearch = e => {
+    e.preventDefault()
+    setQuery(search)
   }
 
   return (
       < div className="MainApp" >
     <h1> Hello world </h1>
-    <form className="search-form">  
+    <form 
+    onSubmit={getSearch}
+    className="search-form"
+    >  
       <input className="search-input" 
       type ="text" 
       value={search} 
