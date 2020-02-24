630bce63e677eea8c1b0a29154c073c76e16063c
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-02-24 22:27:54 +0000

    Chore: add keey for each recipe

--- src/App.js
+++ src/App.js
@@ -34,6 +34,7 @@ const App = () => {
     </form>
     {recipes.map(recipe => (
       <Recipe 
+      key={recipe.recipe.label}
       title={recipe.recipe.label} 
       calories={recipe.recipe.calories} 
       image={recipe.recipe.image}
