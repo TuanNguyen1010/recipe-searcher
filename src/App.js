290784f43d22a1b57822acf65bfe604d82db8db1
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-02-24 21:57:30 +0000

    Chore: save recipe to state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
-import fetchRecipe from './fetchRecipe'
+// import fetchRecipe from './fetchRecipe'
+import Recipe from './Recipe'
+
 
 const App = () => {
-    
+
+  const [recipes, setRecipe] = useState([]);
+  const [search, setSearch] = useState("")
+
   useEffect(() => {
- fetchRecipe()
+  fetchRecipe()
   })
 
+  const fetchRecipe = async () => {
+    const APP_ID = "a3ad555a"
+    const API_KEY = "b9c79644c2f78df9d76f77fc33c0fa24"
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${API_KEY}`).then(response => response.json())
+    setRecipe(response.hits)
+    console.log(response.hits)
+  }
+  
   return (
       < div className="MainApp" >
     <h1> Hello world </h1>
@@ -20,6 +33,10 @@ const App = () => {
       Search
       </button>
     </form>
+    {recipes.map(recipe => (
+      <Recipe title={recipe.recipe.label} calories={recipe.recipe.calories}/>
+
+    ))}
     </div>
   );
 
---
+++ src/Recipe.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const Recipe = () => {
+
+  return(
+  <div> 
+    <h1> Recipe </h1>
+    <img src="" alt="" /> 
+    <h2> calories</h2>
+    <h2> ingredients</h2>
+
+
+  </div>)
+}
+
+export default Recipe;
